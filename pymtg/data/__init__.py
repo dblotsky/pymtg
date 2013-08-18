7bffb4f29aec1e8aeb4a7aaa1fc5a58d9e819dc0
Serghei Filippov <blastowind@gmail.com>
2013-08-18 17:25:36 -0400

    Ensures we always have a settings file to load.

--- pymtg/data/__init__.py
+++ pymtg/data/__init__.py
@@ -1,6 +1,6 @@
 import os.path
 
-from pymtg.utils import json_file_as_dict
+from pymtg.utils import json_file_as_dict, json_dict_to_file
 
 # directories
 DATA_DIR       = os.path.dirname(__file__)
@@ -9,6 +9,7 @@ COLLECTION_DIR = os.path.join(DATA_DIR, "collections")
 # files
 LIBRARY_FILE  = os.path.join(DATA_DIR, "AllSets.json")
 SETTINGS_FILE = os.path.join(DATA_DIR, "preferences.pymtg-settings")
+SETTINGS_DEFAULT_FILE = os.path.join(DATA_DIR, "default-settings.json")
 
 # setting dict
 __settings = None
@@ -23,6 +24,11 @@ def get_setting(setting_name):
 def load_settings():
 
     global __settings
+
+    # Re-create the settings file if it doesn't exist
+    if not os.path.exists(SETTINGS_FILE):
+        defaults = json_file_as_dict(SETTINGS_DEFAULT_FILE)
+        json_dict_to_file(SETTINGS_FILE, defaults)
 
     __settings = json_file_as_dict(SETTINGS_FILE)
 
--- pymtg/utils.py
+++ pymtg/utils.py
@@ -4,3 +4,9 @@ def json_file_as_dict(file_name):
 
     with open(file_name, "r") as json_file:
         return json.load(json_file)
+
+
+def json_dict_to_file(file_name, obj):
+    with open(file_name, "r") as json_file:
+        return json.dump(obj, json_file)
+
